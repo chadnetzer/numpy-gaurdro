2cec76784f3e7cc0d039662a8f42fef6af6d7bed
David Cournapeau <cournape@gmail.com>
2009-05-04 17:39:43 +0900

    Add one function to compat module.

--- numpy/distutils/compat/py2.py
+++ numpy/distutils/compat/py2.py
@@ -1,10 +1,12 @@
-from numpy.distutils import log
+import imp
+
+from numpy.distutils.log import info as loginfo
 
 def try_import_compiler(module_name):
     try:
         __import__ (module_name)
     except ImportError, msg:
-        log.info('%s in numpy.distutils; trying from distutils',
+        loginfo('%s in numpy.distutils; trying from distutils',
                  str(msg))
         module_name = module_name[6:]
         try:
@@ -15,3 +17,12 @@ def try_import_compiler(module_name):
                     module_name)
 
     return module_name
+
+def try_load_module(config_instance, name, *info):
+    try:
+        version_module = imp.load_module(name, *info)
+    except ImportError, msg:
+        config_instance.warn(str(msg))
+        version_module = None
+
+    return version_module
