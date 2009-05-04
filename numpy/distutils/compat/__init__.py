96cc40bfc9a650e606db24949712a76b63a0dfde
David Cournapeau <david@ar.media.kyoto-u.ac.jp>
2009-05-05 02:54:07 +0900

    A few more compat functions.

--- numpy/distutils/compat/__init__.py
+++ numpy/distutils/compat/__init__.py
@@ -1,7 +1,7 @@
 import sys
 if sys.version_info[0] < 3:
     from numpy.distutils.compat.py2 import try_import_compiler, try_load_module, \
-        caught_spawn_command, caught_raise_spawn_command
+        caught_spawn_command, caught_raise_spawn_command, caught_wrap_method, initialize_compiler
 else:
     from numpy.distutils.compat.py3 import try_import_compiler, try_load_module, \
-        caught_spawn_command, caught_raise_spawn_command
+        caught_spawn_command, caught_raise_spawn_command, caught_wrap_method, initialize_compiler
--- numpy/distutils/compat/py2.py
+++ numpy/distutils/compat/py2.py
@@ -1,7 +1,8 @@
 import imp
 import sys
 
-from distutils.errors import DistutilsExecError
+from distutils.errors import DistutilsExecError, CompileError
+
 from numpy.distutils.log import info as loginfo
 
 def try_import_compiler(module_name):
@@ -43,3 +44,27 @@ def caught_raise_spawn_command(func, args, display, raised):
        func(args, display=display)
     except DistutilsExecError, msg:
         raise raised(msg)
+
+def initialize_compiler(command_instance):
+    try:
+        compiler_instance.compiler.initialize()
+    except IOError, e:
+        msg = """\
+============================================================================
+Could not initialize compiler instance: do you have Visual Studio
+installed ? If you are trying to build with mingw, please use python setup.py
+build -c mingw32 instead ). If you have Visual Studio installed, check it is
+correctly installed, and the right version (VS 2008 for python 2.6, VS 2003 for
+2.5, etc...). Original exception was: %s, and the Compiler
+class was %s
+============================================================================""" \
+            % (e, compiler_instance.compiler.__class__.__name__)
+        raise distutils.errors.DistutilsPlatformError(msg)
+
+def caught_wrap_method(method, instance, args, save_compiler):
+    try:
+        ret = method(*((instance,)+args))
+    except (DistutilsExecError,CompileError),msg:
+        instance.compiler = save_compiler
+        raise CompileError
+    return ret
