4af50fe1c93107fcb2ce66a46b2f9f5f913d7a16
David Cournapeau <david@ar.media.kyoto-u.ac.jp>
2009-05-05 13:32:12 +0900

    Make core setup.py runnable under python3.

--- numpy/core/code_generators/generate_umath.py
+++ numpy/core/code_generators/generate_umath.py
@@ -621,7 +621,11 @@ chartotype2 = {'f': 'ff_f',
 # String-handling utilities to avoid locale-dependence.
 
 import string
-UPPER_TABLE = string.maketrans(string.ascii_lowercase, string.ascii_uppercase)
+if sys.version_info[0] < 3:
+    UPPER_TABLE = string.maketrans(string.ascii_lowercase, string.ascii_uppercase)
+else:
+    UPPER_TABLE = string.maketrans(bytes(string.ascii_lowercase, "ascii"), 
+            bytes(string.ascii_uppercase, "ascii"))
 
 def english_upper(s):
     """ Apply English case rules to convert ASCII strings to all upper case.
