99f387d5fec0ed4cf69d60c99a12a317bffe6f50
David Cournapeau <cournape@gmail.com>
2009-06-26 15:16:17 +0900

    Move ma/matrix regressions out of numpy.core tests.

---
+++ numpy/matrx/tests/test_regression.py
@@ -0,0 +1,8 @@
+from numpy.testing import *
+import numpy as np
+
+class TestRegression(TestCase):
+    def test_kron_matrix(self,level=rlevel):
+        """Ticket #71"""
+        x = np.matrix('[1 0; 1 0]')
+        assert_equal(type(np.kron(x,x)),type(x))
